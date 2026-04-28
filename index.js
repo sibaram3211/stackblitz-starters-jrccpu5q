ddee4e030fa8a3ce531e89d78bdd4c84a8020ac2
sibaram3211 <sibaram.nayak3211@gmail.com>
2026-04-28 16:44:02 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { resolve } = require('path');
+
+const app = express();
+const port = 3010;
+
+app.use(express.static('static'));
+
+const DEADLINE = new Date('2026-12-31T23:59:59Z');
+
+app.get('/api/deadline', (req, res) => {
+  const secondsLeft = Math.max(0,Math.floor((DEADLINE.getTime() - Date.now()) / 1000));
+  res.json({ secondsLeft });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+}); 
